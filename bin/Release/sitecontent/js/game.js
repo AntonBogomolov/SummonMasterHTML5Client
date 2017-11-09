e3efbe167d254a0fdfcf4a962797a2a33980f82e
Anton Bogomolov <novemberdevelop@gmail.com>
2017-11-09 16:51:39 +0300

    json fix

--- bin/Release/sitecontent/js/game.js
+++ bin/Release/sitecontent/js/game.js
@@ -24,6 +24,55 @@ function create() {
     s.rotation = 0.5;
 }
 
+function showInstancesList(obj)
+{
+	for (var index in obj.result)
+	{
+		var elem  = obj.result[index];
+		var description = elem.description;
+		var id = elem.objectId;
+		$("#instances").append('<div id="inst'+id+'" onclick="gameGetInstanceInfo('+id+')"><p> id: ' + id + ' descr: '+description+'</p></div>');
+	}
+}
+
+function showInstanceInfo(id, obj)
+{
+	var elem  = obj.result;
+	var width  = elem.width;
+	var height = elem.height;
+
+	$("#inst"+id).append('<div class="instanceInfo"><p>width:'+width+' height:'+height+'</p></div>');
+	$("#inst"+id).append('<div class="instanceControls"><input type="button" value="load" onclick="loadInstance()"/></div>');
+}
+
+function gameGetInstanceInfo(id)
+{
+	var comUrl = 'http://summonmastercore.com?command=getInstanceDescription';	
+	var userId = $("input[name='user_id']").val();
+	var postData = '&user_id='+userId+'&instance_id='+id;
+
+	$.ajax({
+	type: 'POST',
+  	url: comUrl,
+  	data: postData, 
+  	dataType: "binary",
+  	processData: false,
+  	responseType:'arraybuffer',
+  	success: function(msg)
+  	{  
+  		var obj = CBOR.decode(msg);
+  		alert(JSON.stringify(obj, null, 4));
+  		showInstanceInfo(id, obj);
+  		return msg;	
+  	},
+	error: function (msg) 
+  	{  		
+  		alert("error");	
+  		return '';
+  	}
+	});
+}
+
 function gameGetInstancesList()
 {
 	var comUrl = 'http://summonmastercore.com?command=getInstancesList';	
@@ -39,13 +88,13 @@ function gameGetInstancesList()
   	responseType:'arraybuffer',
   	success: function(msg)
   	{  
-  		var obj = CBOR.decode(msg);
-  		alert(JSON.stringify(obj, null, 4));
+		var obj = CBOR.decode(msg);
+		alert(JSON.stringify(obj, null, 4));
+  		showInstancesList(obj);
   		return msg;	
   	},
 	error: function (msg) 
   	{  		
-  		alert(msg);	
   		alert("error");	
   		return '';
   	}
